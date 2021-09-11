12fec7a618962cf548d1ff1990817d73e0bf1e53
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-09-11 18:50:37 +0800

    some fixes

--- shells/fish/command-not-found.fish
+++ shells/fish/command-not-found.fish
@@ -1,3 +1,3 @@
 function __fish_command_not_found_handler --on-event fish_command_not_found
-	/data/data/com.termux/files/usr/libexec/termux/command-not-found $argv[1]
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found $argv
 end

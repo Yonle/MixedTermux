4f5e6a72f5c4009716acd31a1390e3900f7e19cb
Yonle <yonle@protonmail.com>
2022-01-17 14:13:30 +0700

    Add support for exporting environment variables

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ DISTRO=alpine
 # User to execute as
 USER=root
 
+# Keep all environment variables
+KEEPENV=1
+
+# Filter some environment variable that should not store
+# Each variables splitted by name
+FILTER_ENV="SHELL PREFIX PATH LD_PRELOAD LD_LIBRARY_PATH"
+
 # Additional Proot-distro login args. Each flag is separated with space.
 # See "proot-distro login --help" to see proot-distro login arguments
 
@@ -27,8 +34,22 @@ DISTRO_PATH=$PREFIX/var/lib/proot-distro/installed-rootfs/\${DISTRO:-"alpine"}
 ! command -v proot-distro > /dev/null && pkg install -y proot-distro
 ! [ -d \$DISTRO_PATH ] && ! [ -z "\$(ls -A \$DISTRO_PATH)" ] && proot-distro install \${DISTRO:-"alpine"}
 
+environments=""
+
+! [ -z "\$KEEPENV" ] && for i in \$(env); do
+	filtered=0
+	# Don't put some filtered variables
+	for env in \$FILTER_ENV; do
+		(echo \$i | grep -qo \$env) && filtered=1
+	done
+	[ "\$filtered" = 0 ] && environments="\$environments export \$i;"
+done
+
+echo \$environments
 cat <<- END > \$DISTRO_PATH/etc/profile.d/mixedtermux.sh
 #!/usr/bin/env sh
+
+\$environments
 
 cd \$(pwd)
 rm /etc/profile.d/mixedtermux.sh
